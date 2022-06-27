bfc98936ce7b3eb36604730666bca59667bad963
yeaji-helix <96082220+yeaji-helix@users.noreply.github.com>
2022-06-27 16:47:26 -0700

    upgrade cdk version (#92)

--- .projenrc.js
+++ .projenrc.js
@@ -1,6 +1,6 @@
 const { awscdk } = require('projen');
 
-const cdkVersion = '2.25.0';
+const cdkVersion = '2.29.0';
 
 const project = new awscdk.AwsCdkConstructLibrary({
   name: '@myhelix/cdk-watchful',
